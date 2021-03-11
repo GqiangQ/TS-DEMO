2591064532dbd8339115ae40ea331b967a00661e
GQiangQ <740407980@qq.com>
2021-03-11 22:55:05 +0800

    完成

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 {
     var list = [
         ['C', 'N', '%', '÷'],
-        [7, 8, 9, 'x'],
+        [7, 8, 9, '×'],
         [4, 5, 6, '-'],
         [1, 2, 3, '+'],
         [0, '.', '=']
@@ -22,9 +22,106 @@
         buttonBox === null || buttonBox === void 0 ? void 0 : buttonBox.appendChild(div);
     });
     var buttonBox = document.querySelector('#button-box');
+    var types_1 = 'options';
     buttonBox === null || buttonBox === void 0 ? void 0 : buttonBox.addEventListener('click', function (event) {
+        var _a;
         if (event.target instanceof HTMLButtonElement) {
-            console.log(event.target.textContent);
+            var text = event.target.textContent;
+            if (text === null)
+                return;
+            var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
+            var options = ['%', '÷', '×', '-', '+'];
+            var view = document.querySelector("#view");
+            if (view === null)
+                return;
+            if (((_a = view.lastElementChild) === null || _a === void 0 ? void 0 : _a.className) === 'res')
+                view.lastElementChild.remove();
+            if (numbers.includes(text)) {
+                if (types_1 === 'numbers') {
+                    if (view.lastElementChild === null)
+                        return;
+                    var t = view.lastElementChild.textContent;
+                    if ((t === null || t === void 0 ? void 0 : t.includes('.')) && text === '.')
+                        return;
+                    view.lastElementChild.textContent = t + text;
+                }
+                else {
+                    if (text === '.')
+                        return;
+                    var span = document.createElement('span');
+                    span.textContent = text;
+                    span.className = 'numbers';
+                    view.appendChild(span);
+                }
+                // view.textContent = view.textContent + text
+                types_1 = 'numbers';
+            }
+            else if (options.includes(text)) {
+                if (types_1 === 'options')
+                    return;
+                var span = document.createElement('span');
+                span.textContent = text;
+                span.className = 'options';
+                view.appendChild(span);
+                types_1 = 'options';
+            }
+            else if (text === 'N') {
+                if (view.lastElementChild === null)
+                    return;
+                var textContent = view.lastElementChild.textContent;
+                if (textContent && textContent.length > 1 && view.lastElementChild.className !== 'res') {
+                    view.lastElementChild.textContent = textContent.substring(0, textContent.length - 1);
+                }
+                else {
+                    view.lastElementChild.remove();
+                }
+                if (view.lastElementChild === null) {
+                    types_1 = 'options';
+                    return;
+                }
+                types_1 = view.lastElementChild.className;
+            }
+            else if (text === 'C') {
+                view.innerHTML = '';
+                types_1 = 'options';
+            }
+            else {
+                if (view.lastElementChild === null)
+                    return;
+                if (view.lastElementChild.className === 'options')
+                    return;
+                var list_1 = [];
+                var ch = view.children;
+                for (var index = 0; index < ch.length; index++) {
+                    var c = ch[index];
+                    if (c == null)
+                        return;
+                    if (c.className === 'numbers')
+                        list_1.push(Number(ch[index].textContent));
+                    else {
+                        c.textContent && list_1.push(c.textContent);
+                    }
+                }
+                for (var index = 0; index < list_1.length - 2; index = index + 2) {
+                    if (list_1[index + 1] === '×')
+                        list_1[index + 1] = '*';
+                    if (list_1[index + 1] === '÷')
+                        list_1[index + 1] = '/';
+                }
+                // ch.map((item:HTMLElement)=>{
+                // })
+                var res = void 0;
+                try {
+                    res = eval(list_1.join(''));
+                }
+                catch (err) {
+                    res = '错误';
+                }
+                var div = document.createElement('div');
+                div.className = 'res';
+                div.textContent = res.toString();
+                view.appendChild(div);
+            }
         }
     });
 }
--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 {
   let list:Array<Array<number|string>> = [
     ['C', 'N', '%' ,'÷'],
-    [7,8,9,'x'],
+    [7,8,9,'×'],
     [4,5,6,'-'],
     [1,2,3,'+'],
     [0,'.','=']
@@ -22,9 +22,86 @@
     buttonBox?.appendChild(div)
   })
   const buttonBox:Element|null = document.querySelector('#button-box')
+  let types:string|any = 'options'
   buttonBox?.addEventListener('click', (event) => {
     if(event.target instanceof HTMLButtonElement) {
-      console.log(event.target.textContent)
+      const text:string|null = event.target.textContent
+      if(text === null) return
+      const numbers:Array<string> = ['1','2','3','4','5','6','7','8','9','0','.']
+      const options:Array<string> = ['%' ,'÷','×','-','+']
+      let view:HTMLDivElement|null = document.querySelector("#view")
+      if(view === null) return
+      if(view.lastElementChild?.className === 'res') view.lastElementChild.remove()
+      if(numbers.includes(text)) {
+        if (types === 'numbers') {
+          if(view.lastElementChild === null) return
+          let t = view.lastElementChild.textContent
+          if(t?.includes('.') && text === '.') return
+          view.lastElementChild.textContent = t + text
+        } else {
+          if(text === '.') return
+          const span = document.createElement('span')
+          span.textContent = text
+          span.className = 'numbers'
+          view.appendChild(span)
+        }
+        // view.textContent = view.textContent + text
+        types = 'numbers'
+      } else if(options.includes(text)) {
+        if(types === 'options') return
+        const span = document.createElement('span')
+        span.textContent = text
+        span.className = 'options'
+        view.appendChild(span)
+        types = 'options'
+      } else if(text === 'N'){
+        if(view.lastElementChild === null) return
+        let textContent = view.lastElementChild.textContent
+        if(textContent && textContent.length > 1 && view.lastElementChild.className !== 'res') {
+          view.lastElementChild.textContent = textContent.substring(0,textContent.length-1)
+        } else {
+          view.lastElementChild.remove()
+        }
+        if(view.lastElementChild === null){
+          types = 'options'
+          return
+        }
+        types  =  view.lastElementChild.className
+      } else if(text === 'C') {
+        view.innerHTML = ''
+        types = 'options'
+      } else {
+        if(view.lastElementChild === null) return
+        if(view.lastElementChild.className === 'options') return
+        let list:Array<string|number> = []
+        const ch:HTMLCollection = view.children
+        for(let index = 0;index<ch.length;index++){
+          let c = ch[index]
+          if(c == null) return
+          if(c.className === 'numbers')
+            list.push(Number(ch[index].textContent))
+          else{
+            c.textContent && list.push(c.textContent)
+          }
+        }
+        for(let index = 0;index<list.length-2;index = index+2) {
+          if(list[index+1] === '×') list[index+1] = '*'
+          if(list[index+1] === '÷') list[index+1] = '/'
+        }
+        // ch.map((item:HTMLElement)=>{
+
+        // })
+        let res:number|string
+        try {
+          res = eval(list.join('')
+        } catch(err) {
+          res = '错误'
+        }
+        const div = document.createElement('div')
+        div.className = 'res'
+        div.textContent = res.toString()
+        view.appendChild(div)
+      }
     }
   })
 }
